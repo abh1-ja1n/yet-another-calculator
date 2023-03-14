8338190b14403c2be932e411a53c55396a568db7
Abhi Jain <abhi.jjainn@gmail.com>
2023-03-14 23:25:27 +0530

    unit-tests

---
+++ tests/test_scientific_calculator.py
@@ -0,0 +1,38 @@
+import unittest
+import requests
+
+API_URL = 'http://localhost:5000'
+
+class TestScientificCalculator(unittest.TestCase):
+
+    def test_calculate_square_root(self):
+        number = 25
+        expected_result = 5.0
+        response = requests.post(f"{API_URL}/sqrt", json={"number": number})
+        self.assertEqual(response.status_code, 200)
+        self.assertAlmostEqual(response.json()['result'], expected_result)
+
+    def test_calculate_factorial(self):
+        number = 5
+        expected_result = 120
+        response = requests.post(f"{API_URL}/factorial", json={"number": number})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['result'], expected_result)
+
+    def test_calculate_ln(self):
+        number = 2.71828182846
+        expected_result = 1.0
+        response = requests.post(f"{API_URL}/ln", json={"number": number})
+        self.assertEqual(response.status_code, 200)
+        self.assertAlmostEqual(response.json()['result'], expected_result)
+
+    def test_calculate_power(self):
+        number = 2
+        power = 3
+        expected_result = 8
+        response = requests.post(f"{API_URL}/power", json={"number": number, "power": power})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['result'], expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
