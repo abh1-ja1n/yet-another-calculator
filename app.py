a972971b0fc5f6ad0bd2d2a72d122b7b948289fb
Abhi Jain <abhi.jjainn@gmail.com>
2023-03-14 21:35:57 +0530

    scientific calculator

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import math
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/sqrt', methods=['POST'])
+def calculate_square_root():
+    data = request.get_json()
+    num = int(data['number'])
+    result = math.sqrt(num)
+    return jsonify({'result': result})
+
+@app.route('/factorial', methods=['POST'])
+def calculate_factorial():
+    data = request.get_json()
+    num = int(data['number'])
+    result = math.factorial(num)
+    return jsonify({'result': result})
+
+@app.route('/ln', methods=['POST'])
+def calculate_ln():
+    data = request.get_json()
+    num = int(data['number'])
+    result = math.log(num)
+    return jsonify({'result': result})
+
+@app.route('/power', methods=['POST'])
+def calculate_power():
+    data = request.get_json()
+    num = int(data['number'])
+    power = int(data['power'])
+    result = math.pow(num, power)
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ sci-calc/src/Calculator.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import API_URL from './config.js';
+
+
+function Calculator() {
+  const [number, setNumber] = useState('');
+  const [power, setPower] = useState('');
+  const [result, setResult] = useState('');
+
+  const calculateSquareRoot = async () => {
+    try {
+        console.log('fjsd');
+      const response = await axios.post(API_URL + '/sqrt', { number });
+      setResult(response.data.result);
+      console.log('sgss00');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const calculateFactorial = async () => {
+    try {
+      const response = await axios.post(API_URL + '/factorial', { number });
+      setResult(response.data.result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const calculateNaturalLog = async () => {
+    try {
+      const response = await axios.post(API_URL + '/ln', { number });
+      setResult(response.data.result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const calculatePower = async () => {
+    try {
+      const response = await axios.post(API_URL + '/power', { number, power });
+      setResult(response.data.result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Scientific Calculator</h1>
+      <div>
+        <label>Number:</label>
+        <input type="number" value={number} onChange={(e) => setNumber(e.target.value)} />
+      </div>
+      <div>
+        <label>Power:</label>
+        <input type="number" value={power} onChange={(e) => setPower(e.target.value)} />
+      </div>
+      <div>
+        <button onClick={calculateSquareRoot}>Square Root</button>
+        <button onClick={calculateFactorial}>Factorial</button>
+        <button onClick={calculateNaturalLog}>Natural Logarithm</button>
+        <button onClick={calculatePower}>Power</button>
+      </div>
+      <div>
+        <label>Result:</label>
+        <span>{result}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
