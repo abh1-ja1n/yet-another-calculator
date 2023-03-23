b780e9c1ea01e8ffaac789c236cf38c672c6ae09
Abhi Jain <abhi.jjainn@gmail.com>
2023-03-23 18:33:41 +0530

    changes

--- sci-calc-backend/app.py
+++ sci-calc-backend/app.py
@@ -3,47 +3,48 @@ from flask_cors import CORS, cross_origin
 import math
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 
 @app.route('/sqrt', methods=['POST'])
-@cross_origin()
+@cross_origin(origins='*')
 def calculate_square_root():
     data = request.get_json()
     num = float(data['number'])
     result = math.sqrt(num)
     response = jsonify({'result': result})
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    # response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
     return response
 
 @app.route('/factorial', methods=['POST'])
-@cross_origin()
+@cross_origin(origins='*')
 def calculate_factorial():
     data = request.get_json()
     num = float(data['number'])
     result = math.factorial(num)
     response = jsonify({'result': result})
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    # response.headers.add('Access-Control-Allow-Origin', '*')
+    # response.headers.add('Access-Control-Allow-Credentials', '*')
     return response
 
 @app.route('/ln', methods=['POST'])
-@cross_origin()
+@cross_origin(origins='*')
 def calculate_ln():
     data = request.get_json()
     num = float(data['number'])
     result = math.log(num)
     response = jsonify({'result': result})
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    # response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 @app.route('/power', methods=['POST'])
-@cross_origin()
+@cross_origin(origins='*')
 def calculate_power():
     data = request.get_json()
     num = float(data['number'])
     power = float(data['power'])
     result = math.pow(num, power)
     response = jsonify({'result': result})
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    # response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 if __name__ == '__main__':
