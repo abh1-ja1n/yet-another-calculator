48ac5203749b90503fb692bd2d37ccfc64f17950
Abhi Jain <abhi.jjainn@gmail.com>
2023-03-23 20:03:53 +0530

    CORS

--- sci-calc-backend/app.py
+++ sci-calc-backend/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import math
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 
 # @cross_origin(origins=['http://frontend:3000'])
 
 @app.route('/sqrt', methods=['POST'])
+@cross_origin(supports_credentials=True)
 def calculate_square_root():
     data = request.get_json()
     num = float(data['number'])
@@ -17,7 +18,7 @@ def calculate_square_root():
     return response
 
 @app.route('/factorial', methods=['POST'])
-# @cross_origin(origins=['http://frontend:3000'])
+@cross_origin(supports_credentials=True)
 def calculate_factorial():
     data = request.get_json()
     num = float(data['number'])
@@ -28,7 +29,7 @@ def calculate_factorial():
     return response
 
 @app.route('/ln', methods=['POST'])
-# @cross_origin(origins=['http://frontend:3000'])
+@cross_origin(supports_credentials=True)
 def calculate_ln():
     data = request.get_json()
     num = float(data['number'])
@@ -38,7 +39,7 @@ def calculate_ln():
     return response
 
 @app.route('/power', methods=['POST'])
-# @cross_origin(origins=['http://frontend:3000'])
+@cross_origin(supports_credentials=True)
 def calculate_power():
     data = request.get_json()
     num = float(data['number'])
